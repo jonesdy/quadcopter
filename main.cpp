0b5f0b3736a56df1410887e93e029f89a7229da1
Dylan Jones <jonesdyn@gmail.com>
2016-04-17 19:51:02 -0500

    Reference variables!

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #define ECHO 30
 #define TRIG 31
 DistanceSensor sensor(TRIG, ECHO);
+Lcd &lcd = Lcd::getInstance();
 
 void setup()
 {
@@ -17,18 +18,18 @@ void dist_test()
 {
    double distCm = sensor.getDistanceCm();
    double distIn = sensor.getDistanceIn();
-   Lcd::getInstance().home();
-   Lcd::getInstance().print(distCm);
-   Lcd::getInstance().print(" cm");
-   Lcd::getInstance().setCursor(0, 1);
-   Lcd::getInstance().print(distIn);
-   Lcd::getInstance().print(" in");
+   lcd.home();
+   lcd.print(distCm);
+   lcd.print(" cm");
+   lcd.setCursor(0, 1);
+   lcd.print(distIn);
+   lcd.print(" in");
    delay(60);     // 60ms between measurements
 }
 
 void loop()
 {
-   Lcd::getInstance().clear();
+   lcd.clear();
 
    dist_test();
 
